9e576adf6a9bdabc20f67a1e9ddd4cde5c2f3635
znogoud <znogoud@pop-os.localdomain>
2021-08-12 17:34:46 +0300

    145 - Create SearchResult component, 2-columns result page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import SearchContainer from './components/SearchContainer/SearchContainer';
+import SearchResult from './components/SearchResult/SearchResult';
 import TermsAndConditions from './components/Terms/TermsAndConditions';
 import { BrowserRouter, Route } from 'react-router-dom/cjs/react-router-dom.min';
 
@@ -11,6 +12,7 @@ function App() {
       <div className="app-wrapper">
         <Header />
         <Route exact path="/" component={SearchContainer} />
+        <Route path="/result" component={SearchResult} />
         <Route path="/terms" component={TermsAndConditions} />
         <Footer />
       </div>
---
+++ src/components/SearchResult/SearchResult.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import airports from '../../data/test.airports.json';
+import logo from '../../assets/images/airport_logo.png';
+
+import s from './SearchResult.module.css';
+
+const SearchResult = () => {
+    return (
+        <div className={s.result}>
+            <main className={s.searchResultContainer}>
+                {airports.map(port =>
+                    <article className={s.searchResultItem}>
+                        <img src={logo} alt={port.name} className={s.searchResultImage} />
+                        <span className={s.searchResultTitle}>{port.name}</span>
+                        <span className={s.searchResultCountryCode}>{port.id}</span>
+                    </article>
+                )}
+            </main>
+            <div className={s.map}>
+                <img alt="map" src="https://cdn.flightconnections.com/img/opengr.png" />
+            </div>
+        </div>
+    );
+}
+
+export default SearchResult;
