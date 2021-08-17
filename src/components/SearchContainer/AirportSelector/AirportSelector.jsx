44b5747eabf4fe9f251e5563f91f991307ee862c
znogoud <znogoud@pop-os.localdomain>
2021-08-17 19:24:48 +0300

    145 - Edit AirportSelector component

--- src/components/SearchContainer/AirportSelector/AirportSelector.jsx
+++ src/components/SearchContainer/AirportSelector/AirportSelector.jsx
@@ -3,14 +3,22 @@ import React from 'react';
 import s from './AirportSelector.module.css';
 
 import airportFetcher from '../../../fetcher/airportFetcher'
+import { NavLink } from 'react-router-dom';
 
 const AirportSelector = () => {
     return (
         <div className={s.airportSelectorContainer}>
             <div>
                 <div className={s.searchBarContainer}>
-                    <input type="text" class={s.input} id="name" placeholder="Where do you start?" required="" onChange={airportFetcher}/>
-                    <label for="name" class={s.label}>Where do you start?</label>
+                    <input type="text" class={s.input} id="from" placeholder="From?" required="" onChange={airportFetcher} />
+                    <label for="from" class={s.label}>From?</label>
+                    <input type="text" class={s.input} id="to" placeholder="To?" required="" onChange={airportFetcher} />
+                    <label for="to" class={s.label}>To?</label>
+                    <div className={s.buttonContainer}>
+                        <NavLink to="/result">
+                            <button>Search</button>
+                        </NavLink>
+                    </div>
                 </div>
             </div>
         </div>
