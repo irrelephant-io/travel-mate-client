a9259ab2e373aa3514bb4d52dc770a763ab387ca
Eugene 'marengohue' Rebedailo <mokonamodokidesu@gmail.com>
2021-08-18 20:04:09 +0200

    Add airportHook example

--- src/components/SearchContainer/AirportSelector/AirportSelector.jsx
+++ src/components/SearchContainer/AirportSelector/AirportSelector.jsx
@@ -1,34 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import s from './AirportSelector.module.css';
 
-import airportFetcher from '../../../fetcher/airportFetcher'
+import useAirports from '../../../fetcher/airportFetcher';
 import { NavLink } from 'react-router-dom';
 
+
+
 const AirportSelector = () => {
+    const [airports, setSearchString] = useAirports();
 
-    const [from, setFrom] = useState('');
-
-    const getAirports = async (from) => {
-        const res = await airportFetcher(from);
-        // if (!res.ok) {
-        //     throw new Error(`Could not fetch ${from}` + `, received ${res.status}`)
-        // }
-        return res;
+    if (airports == null) {
+	return <span>Loading...</span>;
     }
-
-    getAirports(from)
-        .then(res => console.log(res))
-        .catch(err => console.error('Could not fetch', err))
-
-    // let response = airportFetcher(from);
-    // console.log(response)
-
 
     return (
         <div className={s.airportSelectorContainer}>
             <div>
                 <div className={s.searchBarContainer}>
-                    <input type="text" className={s.input} id="from" placeholder="From?" required="" onChange={e => setFrom(e.target.value)} />
+                    <input
+			type="text"
+			className={s.input}
+			id="from"
+			placeholder="From?"
+			required=""
+			onChange={e => setSearchString(e.target.value)}
+		    />
                     <label htmlFor="from" className={s.label}>From?</label>
                     <input type="text" className={s.input} id="to" placeholder="To?" required="" />
                     <label htmlFor="to" className={s.label}>To?</label>
@@ -39,8 +35,8 @@ const AirportSelector = () => {
                     </div>
                 </div>
             </div>
+	    <div>{airports.map(ap => <span>{ap.name}</span>)}</div>
         </div>
-
     );
 }
 
--- src/fetcher/airportFetcher.js
+++ src/fetcher/airportFetcher.js
@@ -1,16 +1,27 @@
 import airportsMock from './../data/test.airports.json';
 
+import { useState, useEffect } from 'react';
+
+function useAirports() {
+    const [airports, setAirports] = useState(null);
+    
+    useEffect(() => {
+	fetchAirports().then(setAirports);
+    }, []);
+
+    return [airports, (_) => {}];
+}
+
 function fetchAirports(searchString = null) {
-    const value = searchString.target.value.toLowerCase();
     return new Promise((res, rej) => {
         setTimeout(() => {
-            const matchingAirports = value == null
+            const matchingAirports = searchString == null
                 ? airportsMock
-                : airportsMock.filter(port => port.name.toLowerCase().includes(searchString));
+                : airportsMock.filtery(port => port.name.toLowerCase().includes(searchString));
             res(matchingAirports)
         }
         , 1000);
     })
 }
 
-export default fetchAirports;
+export default useAirports;
