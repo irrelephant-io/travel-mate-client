fa231fe3291fd55c355621c4344042ab6dce1f57
znogoud <znogoud@pop-os.localdomain>
2021-08-23 16:26:12 +0300

    Edit the start page

--- src/components/SearchContainer/AirportSelector/AirportSelector.jsx
+++ src/components/SearchContainer/AirportSelector/AirportSelector.jsx
@@ -10,26 +10,39 @@ import { NavLink } from 'react-router-dom';
 const AirportSelector = () => {
     const [airports, setSearchString] = useAirports();
     const setSearchStringDebounced = useDebouncedCallback(setSearchString, 500);
-    
+
     if (airports == null) {
-	return <span>Loading...</span>;
+        return <span>Loading...</span>;
     }
 
     return (
-        <div className={s.airportSelectorContainer}>
+        <form className={s.airportSelectorContainer}>
+            <fieldset>
+                <legend>Find a flexible flight for your next trip.</legend>
+            </fieldset>
             <div>
                 <div className={s.searchBarContainer}>
-                    <input
-			type="text"
-			className={s.input}
-			id="from"
-			placeholder="From?"
-			required=""
-			onChange={e => setSearchStringDebounced(e.target.value)}
-		    />
-                    <label htmlFor="from" className={s.label}>From?</label>
-                    <input type="text" className={s.input} id="to" placeholder="To?" required="" />
-                    <label htmlFor="to" className={s.label}>To?</label>
+                    <div className={s.searchFrom}>
+                        <input
+                            type="text"
+                            className={s.input}
+                            id="from"
+                            placeholder="From?"
+                            required=""
+                            onChange={e => setSearchStringDebounced(e.target.value)}
+                        />
+                        <label htmlFor="from" className={s.label}>From?</label>
+                    </div>
+                    <div className={s.searchTo}>
+                        <input type="text" className={s.input} id="to" placeholder="To?" required="" />
+                        <label htmlFor="to" className={s.label}>To?</label>
+                    </div>
+                    <div className={s.whenTo}>
+                        //calendar
+                    </div>
+                    <div className={s.whenFrom}>
+                    //calendar
+                    </div>
                     <div className={s.buttonContainer}>
                         <NavLink to="/result">
                             <button>Search</button>
@@ -37,8 +50,8 @@ const AirportSelector = () => {
                     </div>
                 </div>
             </div>
-	    <div>{airports.map(ap => <span>{ap.name}</span>)}</div>
-        </div>
+            <div>{airports.map(ap => <span>{ap.name}</span>)}</div>
+        </form>
     );
 }
 
